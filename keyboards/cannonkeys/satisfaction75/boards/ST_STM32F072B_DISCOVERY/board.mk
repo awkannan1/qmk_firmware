a3a3c448e80e5c50ce05aff67348fc8575163201
Andrew Kannan <andrew.kannan@gmail.com>
2020-03-01 15:18:09 -0500

    More formatting

--- keyboards/cannonkeys/satisfaction75/boards/ST_STM32F072B_DISCOVERY/board.mk
+++ keyboards/cannonkeys/satisfaction75/boards/ST_STM32F072B_DISCOVERY/board.mk
@@ -1,5 +1,6 @@
-# List of all the board related files.
-BOARDSRC = $(BOARD_PATH)/boards/ST_STM32F072B_DISCOVERY/board.c
+#List of all the board related files.
+BOARDSRC = $(BOARD_PATH) / boards / ST_STM32F072B_DISCOVERY /
+           board.c
 
-# Required include directories
-BOARDINC = $(BOARD_PATH)/boards/ST_STM32F072B_DISCOVERY
+#Required include directories
+                  BOARDINC = $(BOARD_PATH) / boards / ST_STM32F072B_DISCOVERY
